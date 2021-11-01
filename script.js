a92aadd22c8517024c0030171ee49c1e7b25693c
Vivek-jpg <kumarvivekazio@gmail.com>
2021-11-01 19:54:48 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 const input = document.querySelector('.input');
 const btn= document.querySelector('#btn');
 const output = document.querySelector('.output');
-function CHECKNUMBER(){
+function onclick(){
+    let name = prompt("enter the name of stuedent");
+    name1=input.value;
+    result= name1 + name;
+    output.innerHTML = result;
     
 }
-btn.addEventListener ("click",function)
-{
-
-}
+btn.addEventListener ('click',onclick);
