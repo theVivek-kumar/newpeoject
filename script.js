0054a90955cd9e4229b304646536b947add1ec9a
Vivek-jpg <kumarvivekazio@gmail.com>
2021-11-02 06:16:12 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,9 +3,14 @@ const btn= document.querySelector('#btn');
 const output = document.querySelector('.output');
 function onclick(){
     let name = prompt("enter the name of stuedent");
-    name1=input.value;
-    result= name1 + name;
-    output.innerHTML = result;
+     if(name>=15&&name<=20)
+     {
+         console.log("yes");
+
+     }
+     else{
+         console.log("no");
+     }
     
 }
 btn.addEventListener ('click',onclick);
